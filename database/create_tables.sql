7fe57f7e54b2237b7a5ea312cef3421548a21665
Lukebianbc <cb498@duke.edu>
2022-03-16 21:29:17 -0400

    add create sql

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -15,3 +15,66 @@ CREATE TABLE IF NOT EXISTS accounts (
   last_login TIMESTAMP 
 );
 
+CREATE TABLE IF NOT EXISTS ratings (
+    uuid text NOT NULL PRIMARY KEY,
+    movie_id INTEGER PRIMARY KEY REFERENCES moviedetail (movie_id),
+    rates FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS favourite (
+    uuid text NOT NULL PRIMARY KEY,
+    movie_id INTEGER REFERENCES moviedetail (movie_id)
+);
+
+CREATE TABLE IF NOT EXISTS actorlikes (
+    uuid text NOT NULL PRIMARY KEY,
+    actor_id INTEGER REFERENCES actors (actor_id)
+);
+
+CREATE TABLE IF NOT EXISTS viewhistory (
+    uuid text PRIMARY KEY,
+    movie_id INTEGER REFERENCES moviedetail (movie_id),
+    TIME TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS moviedetail (
+    movie_id INTEGER PRIMARY KEY,
+    adult BOOLEAN NOT NULL,
+    backdrop_path TEXT,
+    budget INTEGER,
+    imdb_id TEXT NOT NULL,
+    title TEXT NOT NULL,
+    overview TEXT,
+    popularity FLOAT,
+    poster_path TEXT,
+    release_date DATE,
+    runtime INTEGER,
+    revenue INTEGER,
+    vote_average FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS genretypes (
+    genre_id INTEGER NOT NULL PRIMARY KEY,
+    genre_name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS moviegenre (
+    movie_id INTEGER NOT NULL PRIMARY KEY REFERENCES moviedetail (movie_id),
+    genre_id INTEGER NOT NULL REFERENCES genretypes (genre_id)
+);
+
+CREATE TABLE IF NOT EXISTS movieactor (
+    movie_id INTEGER NOT NULL PRIMARY KEY REFERENCES moviedetail (movie_id),
+    actor_id INTEGER NOT NULL REFERENCES actors (actor_id)
+);
+
+CREATE TABLE IF NOT EXISTS actors (
+    actor_id INTEGER NOT NULL PRIMARY KEY,
+    birthday DATE,
+    know_for_department TEXT,
+    actor_name TEXT NOT NULL,
+    gender INTEGER NOT NULL,
+    biography TEXT,
+    place_of_birth TEXT,
+    profile_path TEXT
+);
