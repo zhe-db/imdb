e0b0e3e3de87dfb7f575a73b88c16aadee762af2
Lukebianbc <cb498@duke.edu>
2022-03-17 17:46:51 -0400

    create tables successfully

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -15,6 +15,33 @@ CREATE TABLE IF NOT EXISTS accounts (
   last_login TIMESTAMP 
 );
 
+CREATE TABLE IF NOT EXISTS moviedetail (
+    id INTEGER PRIMARY KEY,
+    adult BOOLEAN NOT NULL,
+    backdrop_path TEXT,
+    budget INTEGER,
+    imdb_id TEXT NOT NULL,
+    title TEXT NOT NULL,
+    overview TEXT,
+    popularity FLOAT,
+    poster_path TEXT,
+    release_date DATE,
+    runtime INTEGER,
+    revenue INTEGER,
+    vote_average FLOAT
+);
+
+CREATE TABLE IF NOT EXISTS Cast_info (
+    id INTEGER NOT NULL PRIMARY KEY,
+    birthday DATE,
+    know_for_department TEXT,
+    name TEXT NOT NULL,
+    gender INTEGER NOT NULL,
+    biography TEXT,
+    place_of_birth TEXT,
+    profile_path TEXT
+);
+
 CREATE TABLE IF NOT EXISTS ratings (
     uuid text NOT NULL,
     movie_id INTEGER REFERENCES moviedetail (id),
@@ -41,21 +68,6 @@ CREATE TABLE IF NOT EXISTS viewhistory (
     PRIMARY KEY(uuid, movie_id)
 );
 
-CREATE TABLE IF NOT EXISTS moviedetail (
-    id INTEGER PRIMARY KEY,
-    adult BOOLEAN NOT NULL,
-    backdrop_path TEXT,
-    budget INTEGER,
-    imdb_id TEXT NOT NULL,
-    title TEXT NOT NULL,
-    overview TEXT,
-    popularity FLOAT,
-    poster_path TEXT,
-    release_date DATE,
-    runtime INTEGER,
-    revenue INTEGER,
-    vote_average FLOAT
-);
 
 CREATE TABLE IF NOT EXISTS genretypes (
     id INTEGER NOT NULL PRIMARY KEY,
@@ -72,15 +84,4 @@ CREATE TABLE IF NOT EXISTS Movie_Cast (
     movie_id INTEGER NOT NULL REFERENCES moviedetail (id),
     cast_id INTEGER NOT NULL REFERENCES Cast_info (id),
     PRIMARY KEY(movie_id, cast_id)
-);
-
-CREATE TABLE IF NOT EXISTS Cast_info (
-    id INTEGER NOT NULL PRIMARY KEY,
-    birthday DATE,
-    know_for_department TEXT,
-    name TEXT NOT NULL,
-    gender INTEGER NOT NULL,
-    biography TEXT,
-    place_of_birth TEXT,
-    profile_path TEXT
 );
