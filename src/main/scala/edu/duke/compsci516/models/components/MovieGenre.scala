1c56650d2a6890ea450c87d747257ed55b5addfa
Zhe Wang <zhe.wang1998@duke.edu>
2022-03-20 22:23:58 -0400

    Add movie genre table

---
+++ src/main/scala/edu/duke/compsci516/models/components/MovieGenre.scala
@@ -0,0 +1,43 @@
+package edu.duke.compsci516.models.components
+
+import slick.jdbc.JdbcBackend.Database
+import slick.jdbc.JdbcProfile
+
+import scala.concurrent.Future
+
+import edu.duke.compsci516.models.entity.MovieGenre
+import edu.duke.compsci516.models.database.MovieGenreTable
+
+trait MovieGenreRepositoryComponent {
+  def add(movieGenre: MovieGenre): Future[Int]
+  def deleteMovie(movieId: Int): Future[Int]
+  def deleteGenre(genreId: Int): Future[Int]
+  def getMovieGenres(movie: Int): Future[Seq[Int]]
+  def getMoviesByGenre(genreId: Int): Future[Seq[Int]]
+}
+
+class MovieGenreRepository(db: Database) extends MovieGenreRepositoryComponent {
+  protected val table = MovieGenreTable
+  import table.profile.api._
+  import table.MovieGenreRows
+
+  override def add(movieGenre: MovieGenre): Future[Int] = db.run {
+    MovieGenreRows += movieGenre
+  }
+
+  override def deleteMovie(movieId: Int): Future[Int] = db.run {
+    MovieGenreRows.filter(_.movieId === movieId).delete
+  }
+
+  override def deleteGenre(genreId: Int): Future[Int] = db.run {
+    MovieGenreRows.filter(_.genreId === genreId).delete
+  }
+
+  override def getMovieGenres(movieId: Int): Future[Seq[Int]] = db.run {
+    MovieGenreRows.filter(_.movieId === movieId).map(_.genreId).result
+  }
+
+  override def getMoviesByGenre(genreId: Int): Future[Seq[Int]] = db.run {
+    MovieGenreRows.filter(_.genreId === genreId).map(_.movieId).result
+  }
+}
