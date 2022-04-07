f78bd690b8237ea2622ab42173062cbf8dcf4260
Hadoop <hadoop@movie-recommendation.us-east1-b.c.agile-aleph-345823.internal>
2022-04-07 02:08:35 +0000

    add movie link actor

--- src/main/scala/edu/duke/imdb/core/services/MovieLinksActor.scala
+++ src/main/scala/edu/duke/imdb/core/services/MovieLinksActor.scala
@@ -13,33 +13,77 @@ import _root_.edu.duke.imdb.components.ConfigComponent
 import _root_.edu.duke.imdb.distributedCache.MovieLinksCache
 import _root_.edu.duke.imdb.data.delta.tables.MovieLensLinksDeltaTable
 import _root_.edu.duke.imdb.memStore.ReplicatedCache
+import akka.actor.ActorContext
+import akka.actor.ActorRefFactory
+import akka.actor.typed.scaladsl
+import akka.actor.typed
 
 object MovieLinksActor extends ConfigComponent {
   sealed trait Command
-  val movieLinksReplicatedStore = MovieLinksCache
-  import movieLinksReplicatedStore._
   val linksTable = new MovieLensLinksDeltaTable()
 
-  final case class CreateMovieLinks(replyTo: ActorRef[Boolean]) extends Command
+  final case class CreateMovieLinks() extends Command
   final case class ReadMovieLinks(
       movieIds: Seq[Int],
       replyTo: ActorRef[MovieLinksResponse]
   ) extends Command
   final case class MovieLinksResponse(movieLinks: Seq[Int])
+  private final case class WrappedStoredResponse(
+      response: MovieLinksCache.Response
+  ) extends Command
 
   def apply(): Behavior[Command] =
-    Behaviors.receiveMessage {
-      case CreateMovieLinks(replyTo) => {
-        createLinksFromTable()
-        Behaviors.same
-      }
-      case ReadMovieLinks(movieIds, replyTo) => {
-        Behaviors.same
+    Behaviors.setup { context =>
+      val link2idStore = context.spawn(MovieLinksCache(), "link2idStore")
+      val id2linkStore = context.spawn(MovieLinksCache(), "id2linkStore")
+
+      val storeResponseMapper: ActorRef[MovieLinksCache.Response] =
+        context.messageAdapter(rsp => WrappedStoredResponse(rsp))
+
+      Behaviors.receiveMessage {
+        case CreateMovieLinks() => {
+          createLinksFromTable(link2idStore, id2linkStore, storeResponseMapper)
+          Behaviors.same
+        }
+
+        case ReadMovieLinks(movieIds, replyTo) => {
+          Behaviors.same
+        }
+
+        case wrapped: WrappedStoredResponse =>
+          wrapped.response match {
+            case MovieLinksCache.Cached(key, value) =>
+              println(key, value)
+              Behaviors.same
+          }
       }
     }
 
-  def createLinksFromTable(): Unit = {
-    val df = linksTable.readData()
-    df.foreach { (row) => }
+  def createLinksFromTable(
+      link2idStore: ActorRef[MovieLinksCache.Command],
+      id2linkStore: ActorRef[MovieLinksCache.Command],
+      source: ActorRef[MovieLinksCache.Response]
+  ): Unit = {
+    val df = linksTable.readData().head(10)
+    df.foreach { (row) =>
+      val movieLinkId = row.getString(0)
+      val movieId = row.getString(2)
+      link2idStore ! MovieLinksCache.PutInCache(movieLinkId, movieId)
+      id2linkStore ! MovieLinksCache.PutInCache(movieId, movieLinkId)
+
+      link2idStore ! MovieLinksCache
+        .GetFromCache(movieLinkId, source)
+      id2linkStore ! MovieLinksCache
+        .GetFromCache(movieId, source)
+    }
+  }
+}
+
+object MovieLinksActorObj {
+  def main(args: Array[String]): Unit = {
+    val system: typed.ActorSystem[MovieLinksActor.Command] =
+      typed.ActorSystem(MovieLinksActor(), "movieLinks")
+
+    system ! MovieLinksActor.CreateMovieLinks()
   }
 }
--- src/main/scala/edu/duke/imdb/distributedCache/MovieLinksCache.scala
+++ src/main/scala/edu/duke/imdb/distributedCache/MovieLinksCache.scala
@@ -2,8 +2,112 @@ package edu.duke.imdb.distributedCache
 
 import edu.duke.imdb.memStore.ReplicatedCache
 import akka.cluster.ddata.Replicator
+import akka.actor.typed.ActorRef
+import akka.actor.typed.Behavior
+import akka.actor.typed.scaladsl.Behaviors
+import akka.cluster.ddata.LWWMap
+import akka.cluster.ddata.LWWMapKey
+import akka.cluster.ddata.Replicator._
+import akka.cluster.ddata.SelfUniqueAddress
+import akka.cluster.ddata.typed.scaladsl.DistributedData
+import akka.cluster.ddata.typed.scaladsl.Replicator.{Update, Get}
+import akka.actor.{Actor, Props, ActorLogging}
 
-object MovieLinksCache
+object MovieLinksCache {
+  val name = "movieLinks"
+  val durable = true
+  val writeConsistency = Replicator.WriteLocal
+  val readConsistency = Replicator.ReadLocal
+  sealed trait Command
+  sealed trait Response
+  final case class Cached(key: String, value: Option[String]) extends Response
+
+  final case class PutInCache(key: String, value: String) extends Command
+  final case class GetFromCache(key: String, replyTo: ActorRef[Cached])
+      extends Command
+  final case class Evict(key: String) extends Command
+
+  private sealed trait InternalCommand extends Command
+  private case class InternalGetResponse(
+      key: String,
+      replyTo: ActorRef[Cached],
+      rsp: GetResponse[LWWMap[String, String]]
+  ) extends InternalCommand
+  private case class InternalUpdateResponse(
+      rsp: UpdateResponse[LWWMap[String, String]]
+  ) extends InternalCommand
+
+  def apply(): Behavior[Command] = Behaviors.setup { context =>
+    DistributedData
+      .withReplicatorMessageAdapter[Command, LWWMap[String, String]] {
+        replicator =>
+          implicit val node: SelfUniqueAddress =
+            DistributedData(context.system).selfUniqueAddress
+
+          def dataKey(entryKey: String): LWWMapKey[String, String] =
+            if (durable) {
+              LWWMapKey(
+                "cache-" + name + "-" + math.abs(entryKey.hashCode % 100)
+              )
+            } else {
+              LWWMapKey(
+                "durable-" + name + "-" + math.abs(entryKey.hashCode % 100)
+              )
+            }
+
+          Behaviors.receiveMessage[Command] {
+            case PutInCache(key, value) =>
+              replicator.askUpdate(
+                askReplyTo =>
+                  Update(
+                    dataKey(key),
+                    LWWMap.empty[String, String],
+                    writeConsistency,
+                    askReplyTo
+                  )(_ :+ (key -> value)),
+                InternalUpdateResponse.apply
+              )
+
+              Behaviors.same
+
+            case Evict(key) =>
+              replicator.askUpdate(
+                askReplyTo =>
+                  Update(
+                    dataKey(key),
+                    LWWMap.empty[String, String],
+                    writeConsistency,
+                    askReplyTo
+                  )(_.remove(node, key)),
+                InternalUpdateResponse.apply
+              )
+
+              Behaviors.same
+
+            case GetFromCache(key, replyTo) =>
+              replicator.askGet(
+                askReplyTo => Get(dataKey(key), readConsistency, askReplyTo),
+                rsp => InternalGetResponse(key, replyTo, rsp)
+              )
+
+              Behaviors.same
+
+            case InternalGetResponse(key, replyTo, g @ GetSuccess(_, _)) =>
+              replyTo ! Cached(key, g.dataValue.get(key))
+              Behaviors.same
+
+            case InternalGetResponse(key, replyTo, _: NotFound[_]) =>
+              replyTo ! Cached(key, None)
+              Behaviors.same
+
+            case _: InternalGetResponse    => Behaviors.same // ok
+            case _: InternalUpdateResponse => Behaviors.same // ok
+          }
+      }
+  }
+}
+
+object MovieTestCache
     extends ReplicatedCache[String](
       name = "movieLinks",
       durable = true,
